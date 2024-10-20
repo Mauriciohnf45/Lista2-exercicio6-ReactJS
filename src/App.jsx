1f8ff59d06661688a4f314ed77519c7c915f6d67
Mauriciohnf45 <mh173509@gmail.com>
2024-10-19 22:32:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LinkReact from './components/LinkReact'; // Importando o novo componente
+
+function App() {
+  return (
+    <div>
+      <h2>Visite o Site do ReactJS</h2>
+      <LinkReact /> {/* Incluindo o componente LinkReact */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LinkReact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LinkReact() {
+  return (
+    <a 
+      href="https://reactjs.org" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      Acesse o ReactJS
+    </a>
+  );
+}
+
+export default LinkReact;
